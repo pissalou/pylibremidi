f369d9c8fda78f74530bc84548301977a5181840
pissalou <pascal.mazars@gmail.com>
2025-03-12 13:13:04 +0100

    add `_malloc` to EXPORTED_FUNCTIONS in libremidi.emscripten.cmake

--- cmake/libremidi.emscripten.cmake
+++ cmake/libremidi.emscripten.cmake
@@ -7,4 +7,4 @@ set(LIBREMIDI_HAS_EMSCRIPTEN 1)
 
 set(CMAKE_EXECUTABLE_SUFFIX .html)
 target_compile_definitions(libremidi ${_public} LIBREMIDI_EMSCRIPTEN)
-target_link_options(libremidi ${_public} "SHELL:-s 'EXPORTED_FUNCTIONS=[\"_main\", \"_free\", \"_libremidi_devices_poll\", \"_libremidi_devices_input\"]'")
+target_link_options(libremidi ${_public} "SHELL:-s 'EXPORTED_FUNCTIONS=[\"_malloc\", \"_main\", \"_free\", \"_libremidi_devices_poll\", \"_libremidi_devices_input\"]'")
